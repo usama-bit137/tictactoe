32c1da795effb4b0518eb7fee14ea94ae28ef446
usama <usamaaqeel98@gmail.com>
2023-01-21 12:38:58 +0000

    player factory function changed to Player() class and player, working out logic.

--- script.js
+++ script.js
@@ -1,52 +1,47 @@
-let playersArray = [];
-let squareArray = Array.from(document.querySelectorAll('.item'));
+class Player {
+  constructor(yourName, counter) {
+    this.name = yourName;
+    this.counter = counter;
+  }
 
-function player(name, counter){ 
-    const sayHello = () => console.log(`Hello my name is ${name} my counter is ${counter}`); 
-    return {name, counter, sayHello};
+  sayHello() {
+    console.log(`Hello my name is ${this.yourName} my counter is ${this.counter}`);
+  }
 }
 
 const game = (() => {
+  let playersArray = [];
+  let squareArray = Array.from(document.querySelectorAll('.item'));
+  console.log(squareArray); 
+  let boardState = ['', '', '', '', '', '', '', '', ''];
+  playersArray.push(new Player('usama', 'O'));
+  playersArray.push(new Player('nour', 'X'));
+  const addEventListenerList = (array) => {
+    let playerOne = 0;
 
-   /* Form should go here which enters the name of person. */
-
-    playersArray.push(player('usama', 'O')); 
-    playersArray.push(player('nour', 'X'));
-    
-    let boardState = ["", "", "", 
-                      "", "", "", 
-                      "", "", ""]; 
-
-    var playerOne = 0; 
-    const addEventListenerList = (array) => {
-
-
-        for (let j = 0; j < array.length; j++){
-            array[j].addEventListener('click', () => {
-                
-                if (array[j].textContent == ''){
-                    if ( playerOne == 0 ){
-                        array[j].textContent = `${playersArray[playerOne].counter}`;
-                        boardState[j] = playersArray[playerOne].counter;
-                        //console.log(boardState);
-                        playerOne = 1; 
-                    } else {
-                        array[j].textContent = `${playersArray[playerOne].counter}`; 
-                        boardState[j] = playersArray[playerOne].counter;
-                        //console.log(boardState); 
-                        playerOne = 0;
-                    }
-
-                } else {
-                    alert('Cell occupied... Try another cell!');
-                }
-
-            });
+    array.forEach((element) => {
+      element.addEventListener('click', () => {
+        if (element.textContent === '') {
+          if (playerOne === 0) {
+            element.textContent = `${playersArray[playerOne].counter}`;
+            boardState[element] = playersArray[playerOne].counter;
+            playerOne = 1;
+          } else {
+            element.textContent = `${playersArray[playerOne].counter}`;
+            boardState[element] = playersArray[playerOne].counter;
+            playerOne = 0;
+          }
+        } else {
+          console.log('Cell occupied... Try another cell!');
         }
-    }
+      });
+    });
+  };
 
-    // insert the win logic here
-
-    addEventListenerList(squareArray);
- 
+  // insert the win logic here
+  addEventListenerList(squareArray);
 })();
+/*
+function logic() {
+  if ()
+}*/
