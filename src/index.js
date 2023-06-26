345cc8b90417b4927be99b2805f9c44317831005
usama-bit137 <usamaaqeel98@gmail.com>
2023-06-26 14:11:19 +0100

    Fixed bug: user given retry after clicking occupied tile. Constructed printed template string in an if statement here, to make the win logic function more readable. Improved the way that turns are calculated. Code is simpler using a simple counter-style ++ operator.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import wins from './scripts/wins';
 import Player from './scripts/Player';
 import './style.css';
 
-let boardState = ['', '', '', '', '', '', '', '', ''];
-
 const game = (() => {
+  let boardState = ['', '', '', '', '', '', '', '', ''];
   let playersArray = [];
+
   let squareArray = Array.from(document.querySelectorAll('.item'));
+
   playersArray.push(new Player('player 1', 'O', '1'));
   playersArray.push(new Player('player 2', 'X', '2'));
 
@@ -15,21 +16,22 @@ const game = (() => {
 
     array.forEach((element) => {
       element.addEventListener('click', () => {
+        turn = turn % 2;
         if (element.textContent === '') {
-          if (turn === 0) {
-            playersArray[turn].move(array, element, boardState);
-            turn = 1;
-          } else {
-            playersArray[turn].move(array, element, boardState);
-            turn = 0;
-          }
+          // if a board space is empty, then add the player's counter
+          playersArray[turn].move(array, element, boardState);
         } else {
+          // else alert user to try again
           alert('Cell occupied... Try another cell!');
+          turn++;
         }
+        const winner = wins(boardState);
 
-        if (boardState[element] !== '') {
-          wins(boardState);
+        if (winner) {
+          console.log(`${winner} is the winner!`);
+          // turn this into a querySelector when ready!
         }
+        turn++;
       });
     });
   };
