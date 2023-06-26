cfa29604ed3d5e928158f0836500337f4ab3d3da
usama-bit137 <usamaaqeel98@gmail.com>
2023-06-26 19:11:12 +0100

    prompts added in game start asking player names.

--- src/index.js
+++ src/index.js
@@ -2,15 +2,23 @@ import addEventListenerList from './scripts/addEventListenerList';
 import Player from './scripts/Player';
 import './style.css';
 
-let boardState = ['', '', '', '', '', '', '', '', ''];
 let playersArray = [];
 
-const playerOneName = document.querySelector("[name = 'player-one']");
-playersArray.push(new Player('usama', 'O', '1'));
-playersArray.push(new Player('nour', 'X', '2'));
-
 (function () {
-  let array = Array.from(document.querySelectorAll('.item'));
-  addEventListenerList(array, playersArray, boardState);
-  console.log(`input: ${playerOneName.textContent}`);
+  // This works but tedious
+  // let playerOne = prompt('Player One');
+  // let playerTwo = prompt('Player Two');
+
+  let playerOne = 'usama';
+  let playerTwo = 'nour';
+
+  if (playerOne && playerTwo) {
+    playersArray.push(new Player(playerOne, 'O', '1'));
+    playersArray.push(new Player(playerTwo, 'X', '2'));
+
+    let boardState = ['', '', '', '', '', '', '', '', ''];
+
+    let array = Array.from(document.querySelectorAll('.item'));
+    addEventListenerList(array, playersArray, boardState);
+  }
 })();
