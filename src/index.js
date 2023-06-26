85cb7b058040abeda10093833bfcdc61e4bf6e61
usama-bit137 <usamaaqeel98@gmail.com>
2023-06-26 15:07:35 +0100

    game module removed in favour of IIFE. This stops the global namespace being polluted and it overall more sleek in design. The IIFE only queries the grid items in HTML and calls the addEventListenerList() function.

--- src/index.js
+++ src/index.js
@@ -1,40 +1,16 @@
-import wins from './scripts/wins';
+import addEventListenerList from './scripts/addEventListenerList';
 import Player from './scripts/Player';
 import './style.css';
 
-const game = (() => {
-  let boardState = ['', '', '', '', '', '', '', '', ''];
-  let playersArray = [];
+let boardState = ['', '', '', '', '', '', '', '', ''];
+let playersArray = [];
 
-  let squareArray = Array.from(document.querySelectorAll('.item'));
+playersArray.push(new Player('player 1', 'O', '1'));
+playersArray.push(new Player('player 2', 'X', '2'));
 
-  playersArray.push(new Player('player 1', 'O', '1'));
-  playersArray.push(new Player('player 2', 'X', '2'));
-
-  const addEventListenerList = (array) => {
-    let turn = 0;
-
-    array.forEach((element) => {
-      element.addEventListener('click', () => {
-        turn = turn % 2;
-        if (element.textContent === '') {
-          // if a board space is empty, then add the player's counter
-          playersArray[turn].move(array, element, boardState);
-        } else {
-          // else alert user to try again
-          alert('Cell occupied... Try another cell!');
-          turn++;
-        }
-        const winner = wins(boardState);
-
-        if (winner) {
-          console.log(`${winner} is the winner!`);
-          // turn this into a querySelector when ready!
-        }
-        turn++;
-      });
-    });
-  };
-
-  addEventListenerList(squareArray);
+(function () {
+  // IIFE used in place of module.
+  // does not pollute global namespace
+  let array = Array.from(document.querySelectorAll('.item'));
+  addEventListenerList(array, playersArray, boardState);
 })();
