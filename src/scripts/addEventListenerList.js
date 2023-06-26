a4d05e33458ecb81d60ac6fffe6ddbe10707302d
usama-bit137 <usamaaqeel98@gmail.com>
2023-06-26 21:30:11 +0100

    Implemented logic to solve for a Tie.

--- src/scripts/addEventListenerList.js
+++ src/scripts/addEventListenerList.js
@@ -1,4 +1,5 @@
 import whoWon from './whoWon';
+
 const whosTurn = document.querySelector('#player-turn');
 const addEventListenerList = (boardArray, playersArray, boardState) => {
   let turn = 0;
@@ -7,20 +8,24 @@ const addEventListenerList = (boardArray, playersArray, boardState) => {
   boardArray.forEach((element) => {
     element.addEventListener('click', () => {
       turn = turn % 2;
+
       if (element.textContent === '') {
         whosTurn.append();
         playersArray[turn].move(boardArray, element, boardState);
+        console.log(boardState);
       } else {
         alert('Cell occupied... Try another cell!');
         turn++;
       }
+
       whosTurn.textContent = `${playersArray[(turn + 1) % 2].name}'s turn`;
 
       const winner = whoWon(boardState, playersArray);
-
       if (winner) {
         console.log(`${winner} is the winner!`);
-        whosTurn.textContent = `${winner} won!`;
+        whosTurn.textContent = `${winner} won!`; // end game?
+      } else if (!boardState.includes('')) {
+        whosTurn.textContent = 'Tie!';
       }
       turn++;
     });
