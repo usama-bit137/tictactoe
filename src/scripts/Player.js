90be577427974d9f19b46604ba77a40e8386b552
usama-bit137 <usamaaqeel98@gmail.com>
2023-06-26 15:08:01 +0100

    Comments added for later revival.

--- src/scripts/Player.js
+++ src/scripts/Player.js
@@ -1,15 +1,20 @@
 export default class Player {
+  // what should this do?
+  // Is this SOLID?
   constructor(yourName, yourCounter, yourOrder) {
     this.name = yourName;
     this.counter = yourCounter;
     this.order = yourOrder;
   }
   sayHi() {
+    // This should be a querySelector which updates a div after user inputs their name:
     console.log(
       `${this.yourName} is Player ${this.yourOrder}. Your counter is ${this.yourCounter}`
     );
   }
+
   move(array, element, boardState) {
+    // this is fine:
     element.textContent = `${this.counter}`;
     boardState[array.indexOf(element)] = this.counter;
   }
