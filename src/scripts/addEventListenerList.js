edea2687d76b7657d83a3ec42b03c37ba880cf5f
usama-bit137 <usamaaqeel98@gmail.com>
2023-06-26 19:12:38 +0100

    script added which shows which player's turn it us and updates on win, showing the winner.

--- src/scripts/addEventListenerList.js
+++ src/scripts/addEventListenerList.js
@@ -1,24 +1,26 @@
 import whoWon from './whoWon';
-
+const whosTurn = document.querySelector('#player-turn');
 const addEventListenerList = (boardArray, playersArray, boardState) => {
   let turn = 0;
+
+  whosTurn.textContent = `${playersArray[0].name} is starting`;
   boardArray.forEach((element) => {
     element.addEventListener('click', () => {
       turn = turn % 2;
       if (element.textContent === '') {
+        whosTurn.append();
         playersArray[turn].move(boardArray, element, boardState);
-        // console.log(playersArray[turn].name);
       } else {
         alert('Cell occupied... Try another cell!');
         turn++;
       }
+      whosTurn.textContent = `${playersArray[(turn + 1) % 2].name}'s turn`;
 
       const winner = whoWon(boardState, playersArray);
 
       if (winner) {
         console.log(`${winner} is the winner!`);
-        // turn this into a querySelector when ready!
-        // alert(`${winner} is the winner!`);
+        whosTurn.textContent = `${winner} won!`;
       }
       turn++;
     });
