174a648668ff5fd8d7ec9feba0417948b044a819
usama-bit137 <usamaaqeel98@gmail.com>
2023-06-26 14:12:42 +0100

    Removed console.log template strings in favour of returning the winner of the game. console.logs moved to the main game module.

--- src/scripts/wins.js
+++ src/scripts/wins.js
@@ -1,24 +1,26 @@
 const wins = (array) => {
+  let winner;
   if (array[0] === array[1] && array[0] === array[2]) {
     // Horizontals
-    console.log(`${array[0]} is the winner!`);
+    winner = array[0];
   } else if (array[3] === array[4] && array[3] === array[5]) {
-    console.log(`${array[3]} is the winner!`);
+    winner = array[3];
   } else if (array[6] === array[7] && array[6] === array[8]) {
-    console.log(`${array[6]} is the winner!`);
+    winner = array[6];
   } else if (array[0] === array[3] && array[0] === array[6]) {
     // Verticals
-    console.log(`${array[0]} is the winner!`);
+    winner = array[0];
   } else if (array[1] === array[4] && array[1] === array[7]) {
-    console.log(`${array[1]} is the winner!`);
+    winner = array[1];
   } else if (array[3] === array[5] && array[3] === array[8]) {
-    console.log(`${array[3]} is the winner!`);
+    winner = array[3];
   } else if (array[0] === array[4] && array[0] === array[8]) {
     // Diagonals
-    console.log(`${array[0]} is the winner!`);
+    winner = array[0];
   } else if (array[2] === array[4] && array[2] === array[6]) {
-    console.log(`${array[0]} is the winner!`);
+    winner = array[0];
   }
+  return winner;
 };
 
 export default wins;
