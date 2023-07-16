d838291853549a3dabf7130d0daddb914917218f
usama-bit137 <usamaaqeel98@gmail.com>
2023-07-16 23:54:41 +0100

    Fixed the bug where '' board item was being reported as the winning counter.

--- src/scripts/whoWon.js
+++ src/scripts/whoWon.js
@@ -1,25 +1,54 @@
 const wins = (board) => {
   // The possible wins. returns the winning counter
   let winCounter;
-  if (board[0] === board[1] && board[0] === board[2]) {
-    // Horizontals
+
+  // HORIZONTALS
+  if (board[0] !== '' && board[0] === board[1] && board[0] === board[2]) {
     winCounter = board[0];
-  } else if (board[3] === board[4] && board[3] === board[5]) {
+  } else if (
+    board[3] !== '' &&
+    board[3] === board[4] &&
+    board[3] === board[5]
+  ) {
     winCounter = board[3];
-  } else if (board[6] === board[7] && board[6] === board[8]) {
+  } else if (
+    board[6] !== '' &&
+    board[6] === board[7] &&
+    board[6] === board[8]
+  ) {
     winCounter = board[6];
-  } else if (board[0] === board[3] && board[0] === board[6]) {
-    // Verticals
+    // VERTICALS
+  } else if (
+    board[0] !== '' &&
+    board[0] === board[3] &&
+    board[0] === board[6]
+  ) {
     winCounter = board[0];
-  } else if (board[1] === board[4] && board[1] === board[7]) {
+  } else if (
+    board[1] !== '' &&
+    board[1] === board[4] &&
+    board[1] === board[7]
+  ) {
     winCounter = board[1];
-  } else if (board[3] === board[5] && board[3] === board[8]) {
-    winCounter = board[3];
-  } else if (board[0] === board[4] && board[0] === board[8]) {
-    // Diagonals
+  } else if (
+    board[2] !== '' &&
+    board[2] === board[5] &&
+    board[2] === board[8]
+  ) {
+    winCounter = board[2];
+    // DIAGONALS
+  } else if (
+    board[0] !== '' &&
+    board[0] === board[4] &&
+    board[0] === board[8]
+  ) {
     winCounter = board[0];
-  } else if (board[2] === board[4] && board[2] === board[6]) {
-    winCounter = board[0];
+  } else if (
+    board[2] !== '' &&
+    board[2] === board[4] &&
+    board[2] === board[6]
+  ) {
+    winCounter = board[2];
   }
   return winCounter;
 };
